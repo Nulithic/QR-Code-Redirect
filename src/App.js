09346705446f9d3d59e98ba3e0026ed0190844db
nulithic <admin@nulithic.dev>
2021-07-16 18:30:16 -0700

    Test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 function App() {
+  function getMobileOperatingSystem() {
+    var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+    if (/android/i.test(userAgent)) {
+      window.location.replace("https://play.google.com/store/apps/details?id=cc.smarnet.wippyprint&hl=en_US&gl=US");
+    }
+
+    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+      window.location.replace("https://apps.apple.com/us/app/wippyprint/id1561908354");
+    }
+  }
+
   return (
     <div className="App">
       <p>Redirect</p>
+      {getMobileOperatingSystem}
     </div>
   );
 }
